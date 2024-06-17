c5bca461e4eec8877dc564bb4eeb9e81eef64b0d
IrakliTabatadze <irakli.tabatadze.80@gmail.com>
2024-06-17 22:20:10 +0400

    Lecture 1

---
+++ Lecture 1/main.py
@@ -0,0 +1,20 @@
+
+
+number1 = int(input("Please enter first number: "))
+number2 = int(input("Please enter second number: "))
+
+add = number1 + number2
+sub = number1 - number2
+mul = number1 * number2
+div = number1 / number2
+power = number1 ** number2
+slash = number1 // number2
+nashti = number1 % number2
+
+print("Add: ", add)
+print("Sub: ", sub)
+print("Mul: ", mul)
+print("Div: ", div)
+print("Power: ", power)
+print("Slash: ", slash)
+print("Nashti: ", nashti)
